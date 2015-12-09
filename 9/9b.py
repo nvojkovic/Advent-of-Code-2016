a4fd48bd0b05fea6ece8ac1f340686e874a3f050
Nikola Vojkovic <nikola.vojkovic@live.com>
2015-12-09 10:37:34 +0100

    Solutions for day 9

---
+++ 9/9b.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+locations = set()
+distances = dict()
+
+with open("input.txt") as f:
+	for line in f.read().splitlines():
+		first, second = line.split(" to ")
+		second, distance = second.split(" = ")
+		locations.add(first)
+		locations.add(second)
+		distances[(first, second)] = int(distance)
+		distances[(second, first)] = int(distance)
+
+result = 0
+for x in permutations(list(locations)):
+	sum = 0
+	for i in range(len(x)-1):
+		sum += distances[(x[i], x[i+1])]
+	result = max(result, sum)
+print(result)
