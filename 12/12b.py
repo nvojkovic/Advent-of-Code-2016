979d148af7b0e417b22457b85018e21ebce1603e
Nikola Vojkovic <nikola.vojkovic@live.com>
2015-12-13 00:17:27 +0100

    Add solutions for day 12

---
+++ 12/12b.py
@@ -0,0 +1,28 @@
+import json
+
+sum=0
+
+def addIfInt(x):
+	if type(x) is int:
+		global sum
+		sum += x
+
+def parse(code):
+	if type(code) is list :
+		for x in code:
+			if type(x) in [dict,list]:
+				parse(x)
+			else:
+				addIfInt(x)
+	elif type(code) is dict and "red" not in code.keys() and "red" not in code.values():
+		for key, value in code.items():
+			addIfInt(key)
+			if type(value) in [dict,list]:
+				parse(value)
+			else:
+				addIfInt(value)
+
+with open("input.txt") as f:
+	code = f.read()
+	parse(json.loads(code))
+	print(sum)
